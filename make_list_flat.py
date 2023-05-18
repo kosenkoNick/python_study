1a3779ced15100cc4c09bbc5a4d7f09e8157667b
mkosenko <mkosenko@agileassets.com>
2023-05-18 11:59:22 +0300

    Added the docstring, return type and refactored the code

--- make_list_flat.py
+++ make_list_flat.py
@@ -1,13 +1,15 @@
-dim_list = [[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]
-
-def flattern(array : list):
+def flattern(data : list) -> list:
+    '''
+    The func takes a list, containing list among aother elements, 
+    and returns a one-level list
+    Example: [[2], 5, [[2], [5, 4]]] -> [2, 5, 2, 5, 4]
+    '''
     result = []
-    for el in array:
-        if type(el) == list:
+    for el in data:
+        if isinstance(el, list):
             el = flattern(el)
             result += el
         else:
             result.append(el)
 
-        
     return result
