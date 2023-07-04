2851ae12fe489857815ba0d7a494d38433a8c59c
mkosenko <mkosenko@agileassets.com>
2023-07-05 02:22:43 +0300

    ...

--- iter_gen_homework/iterators.py
+++ iter_gen_homework/iterators.py
@@ -71,18 +71,18 @@ class PingPongIterator:
 if __name__ == "__main__":
     a = [0, 1, 2]
 
-    # iterator = SimpleIterator(a)
-    # b = list(iterator)
-    # assert b == [0, 1, 2]
+    iterator = SimpleIterator(a)
+    b = list(iterator)
+    assert b == [0, 1, 2]
 
     iterator = ReversedIterator(a)
     b = list(iterator)
     assert b == [2, 1, 0]
-    #
-    # iterator = CycleIterator(a)
-    # b = [next(iterator) for _ in range(14)]
-    # assert b == [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1]
-    #
-    # iterator = PingPongIterator(a)
-    # b = [next(iterator) for _ in range(14)]
-    # assert b == [0, 1, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1, 0, 1]
+
+    iterator = CycleIterator(a)
+    b = [next(iterator) for _ in range(14)]
+    assert b == [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1]
+
+    iterator = PingPongIterator(a)
+    b = [next(iterator) for _ in range(14)]
+    assert b == [0, 1, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1, 0, 1]
